bdb70b156d9015842b07b86e5ff1d95d50baca5e
backwardspy <backwardspy@pigeon.life>
2023-11-04 14:56:39 +0000

    feat: replace torus with ring model, add skybox

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,22 @@
 #![deny(clippy::all, clippy::pedantic, clippy::nursery, clippy::unwrap_used)]
 // otherwise we'll get warnings on all Res/ResMut parameters.
 #![allow(clippy::needless_pass_by_value)]
-use std::f32::consts::PI;
+// intentional cast truncation & sign loss is pretty common in gamedev.
+#![allow(clippy::cast_possible_truncation, clippy::cast_sign_loss)]
 
 use bevy::{
-    prelude::{
-        shape, App, Assets, Camera3dBundle, ClearColor, Color, Commands, Component,
-        DirectionalLightBundle, EulerRot, Mesh, PbrBundle, Quat, Query, Res, ResMut,
-        StandardMaterial, Startup, Transform, Update, Vec3,
+    asset::LoadState,
+    core_pipeline::{
+        experimental::taa::{TemporalAntiAliasBundle, TemporalAntiAliasPlugin},
+        Skybox,
     },
+    prelude::{
+        debug, App, AssetServer, Assets, Camera3dBundle, ClearColor, Color, Commands, Component,
+        DirectionalLight, DirectionalLightBundle, EnvironmentMapLight, EulerRot, Handle, Image,
+        Quat, Query, Res, ResMut, Resource, Startup, Transform, Update, Vec3,
+    },
+    render::render_resource::{TextureViewDescriptor, TextureViewDimension},
+    scene::SceneBundle,
     time::Time,
     DefaultPlugins,
 };
@@ -16,41 +24,77 @@ use bevy::{
 #[derive(Component)]
 struct Spin;
 
-fn setup_world(
-    mut commands: Commands,
-    mut meshes: ResMut<Assets<Mesh>>,
-    mut materials: ResMut<Assets<StandardMaterial>>,
-) {
-    commands.spawn(Camera3dBundle {
-        transform: Transform::from_xyz(0.0, 1.5, 3.0).looking_at(Vec3::ZERO, Vec3::Y),
-        ..Default::default()
-    });
+#[derive(Component)]
+struct OrbitCamera;
+
+#[derive(Resource)]
+struct Cubemaps(Vec<Handle<Image>>);
+
+fn setup_world(mut commands: Commands, mut cubemaps: ResMut<Cubemaps>, assets: Res<AssetServer>) {
+    let rang = assets.load("ring.glb#Scene0");
+    let sky = assets.load("sky.png");
+
+    cubemaps.0.push(sky.clone());
+
+    commands.spawn((
+        Camera3dBundle {
+            transform: Transform::from_xyz(0.0, 0.5, 1.5).looking_at(Vec3::ZERO, Vec3::Y),
+            ..Default::default()
+        },
+        TemporalAntiAliasBundle::default(),
+        Skybox(sky.clone()),
+        EnvironmentMapLight {
+            diffuse_map: sky.clone(),
+            specular_map: sky,
+        },
+        OrbitCamera,
+    ));
 
     commands.spawn(DirectionalLightBundle {
+        directional_light: DirectionalLight {
+            color: Color::hsl(30.0, 0.5, 0.8),
+            ..Default::default()
+        },
         transform: Transform::from_rotation(Quat::from_euler(EulerRot::XYZ, 1.2, 2.8, 0.0)),
         ..Default::default()
     });
 
-    let mesh = meshes.add(
-        shape::Torus {
-            radius: 1.0,
-            ring_radius: 0.1,
-            subdivisions_segments: 256,
-            subdivisions_sides: 64,
-        }
-        .into(),
-    );
-    let mat = materials.add(Color::hsl(35.0, 0.77, 0.49).into());
-
-    commands
-        .spawn(PbrBundle {
-            mesh,
-            material: mat,
-            transform: Transform::from_xyz(0.0, 0.0, 0.0)
-                .with_rotation(Quat::from_rotation_x(PI / 2.0)),
+    commands.spawn((
+        SceneBundle {
+            scene: rang,
+            transform: Transform::from_xyz(0.0, 0.0, 0.0).with_rotation(Quat::from_rotation_x(0.2)),
             ..Default::default()
-        })
-        .insert(Spin);
+        },
+        Spin,
+    ));
+}
+
+fn setup_cubemaps(
+    mut cubemaps: ResMut<Cubemaps>,
+    mut images: ResMut<Assets<Image>>,
+    assets: Res<AssetServer>,
+) {
+    cubemaps.0.retain(|handle| {
+        if assets.get_load_state(handle) == LoadState::Loaded {
+            debug!("cubemap texture loaded, setting up sampler...");
+            let image = images
+                .get_mut(handle)
+                .expect("cubemap image handle is valid");
+
+            if image.texture_descriptor.array_layer_count() == 1 {
+                // the cubemap can't have only one image element, so we have to tell it to reinterpret the image as a stacked array.
+                image.reinterpret_stacked_2d_as_array(image.aspect_2d() as u32);
+                image.texture_view_descriptor = Some(TextureViewDescriptor {
+                    dimension: Some(TextureViewDimension::Cube),
+                    ..Default::default()
+                });
+            }
+
+            false // loaded, drop
+        } else {
+            true // not loaded yet, retain
+        }
+    });
 }
 
 fn spin(mut spinners: Query<(&Spin, &mut Transform)>, time: Res<Time>) {
@@ -59,11 +103,22 @@ fn spin(mut spinners: Query<(&Spin, &mut Transform)>, time: Res<Time>) {
     }
 }
 
+fn orbit(mut orbiters: Query<(&OrbitCamera, &mut Transform)>, time: Res<Time>) {
+    for (_, mut transform) in &mut orbiters {
+        transform.rotate_around(
+            Vec3::ZERO,
+            Quat::from_rotation_y(-time.delta_seconds() / 15.0),
+        );
+    }
+}
+
 fn main() {
     App::new()
-        .insert_resource(ClearColor(Color::hsl(220.0, 0.23, 0.95)))
+        .insert_resource(ClearColor(Color::rgb(0.10, 0.08, 0.06)))
+        .insert_resource(Cubemaps(vec![]))
         .add_plugins(DefaultPlugins)
+        .add_plugins(TemporalAntiAliasPlugin)
         .add_systems(Startup, setup_world)
-        .add_systems(Update, spin)
+        .add_systems(Update, (setup_cubemaps, spin, orbit))
         .run();
 }
